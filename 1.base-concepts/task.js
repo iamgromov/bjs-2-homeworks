981477f4eec5812a6da93e4243a823ad450c1841
iamgromov <iamgromov@gmail.com>
2023-03-03 10:44:46 +0300

    Внесены правки в условные конструкции

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,9 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = (Math.pow(b, 2) - 4 * a * c);
   
-  if (d < 0) return arr;
+  if (d < 0) {
+    return arr;
+  }
 
   if (d === 0) {
     arr.push(-b / (2 * a));
@@ -18,12 +20,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (
-    typeof percent != "number" ||
-    typeof contribution != "number" ||
-    typeof amount != "number" ||
-    typeof countMonths != "number"
-  ) return false;
+  if (typeof percent != "number" || typeof contribution != "number" || typeof amount != "number" || typeof countMonths != "number") {
+    return false;
+  }
 
   let p = percent / 100 / 12;
   let s = amount - contribution;
