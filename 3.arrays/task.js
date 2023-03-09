a84dfb7113bbd6c034b75b1b80c0d693c35714e1
iamgromov <iamgromov@gmail.com>
2023-03-09 19:28:46 +0300

    Третье задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => acc + item / arr.length, 0);
 }
